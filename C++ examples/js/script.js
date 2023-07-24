fca38a319cd657c1f441ebf0973946a52e34cd13
jonasRower <119448235+jonasRower@users.noreply.github.com>
2023-07-24 12:49:34 +0200

    Add files via upload

---
+++ C++ examples/js/script.js
@@ -0,0 +1,401 @@
+
+function skryjVsechnyOstatniHtml(){
+
+    $('#pole').hide();
+    $('#forSmyckaPole').hide();
+    $('#velikostPole').hide();
+    $('#pointerNaPole').hide();
+    $('#struktury').hide();
+    $('#strlenSizeof').hide();
+    $('#substring').hide();
+    $('#vypisZnaku').hide();
+    $('#zmenaPismene').hide();
+    $('#poleStruktur').hide();
+    $('#adresy').hide();
+    $('#pointer').hide();
+    $('#trida').hide();
+    $('#tridaMetoda').hide();
+    $('#tridaKonstruktor').hide();
+    $('#nactiTxtCisla').hide();
+    $('#vystupDoTxt').hide();
+    $('#tridaGeter').hide();
+    $('#tridaSeter').hide();
+    $('#tridaPole').hide();
+    $('#tridaDveTridy').hide();
+    $('#tridaKonstructorPointer').hide();
+    $('#tridaPredani2Dpole').hide();
+    $('#delkaPole').hide();
+    $('#delkaPole2D').hide();
+    $('#delkaPole2DNew').hide();
+    $('#static').hide();
+    $('#poleMalloc').hide();
+    $('#staticPoradiVolani').hide();
+    $('#staticZapisDoPole').hide();
+    $('#poleZnakuMalloc').hide();
+    $('#strlenSizeOfStringArrChar').hide();
+    $('#reference').hide();
+    $('#staticPoleStringu').hide();
+    $('#poleAdresPorovnani').hide();
+    $('#referencePole').hide();
+    $('#poleAdres2DPorovnani').hide();
+    $('#poleChar2DPorovnaniAdres').hide();
+    $('#staticIntStringChar').hide();
+
+    $('#poleButt').css("color", "yellow");
+    $('#forSmyckaPoleButt').css("color", "yellow");
+    $('#velikostPoleButt').css("color", "yellow");
+    $('#pointerNaPoleButt').css("color", "yellow");
+    $('#strukturyButt').css("color", "yellow");
+    $('#strlenSizeofButt').css("color", "yellow");
+    $('#substringButt').css("color", "yellow");
+    $('#vypisZnakuButt').css("color", "yellow");
+    $('#zmenaPismeneButt').css("color", "yellow");
+    $('#poleStrukturButt').css("color", "yellow");
+    $('#adresyButt').css("color", "yellow");
+    $('#pointerButt').css("color", "yellow");
+    $('#tridaButt').css("color", "yellow");
+    $('#tridaMetodaButt').css("color", "yellow");
+    $('#tridaKonstruktorButt').css("color", "yellow");
+    $('#nactiTxtCislaButt').css("color", "yellow");
+    $('#vystupDoTxtButt').css("color", "yellow");
+    $('#tridaGeterButt').css("color", "yellow");
+    $('#tridaSeterButt').css("color", "yellow");
+    $('#tridaPoleButt').css("color", "yellow");
+    $('#tridaDveTridyButt').css("color", "yellow");
+    $('#tridaKonstructorPointerButt').css("color", "yellow");
+    $('#tridaPredani2DpoleButt').css("color", "yellow");
+    $('#delkaPoleButt').css("color", "yellow");
+    $('#delkaPole2DButt').css("color", "yellow");
+    $('#delkaPole2DNewButt').css("color", "yellow");
+    $('#staticButt').css("color", "yellow");
+    $('#poleMallocButt').css("color", "yellow");
+    $('#staticPoradiVolaniButt').css("color", "yellow");
+    $('#staticZapisDoPoleButt').css("color", "yellow");
+    $('#poleZnakuMallocButt').css("color", "yellow");
+    $('#strlenSizeOfStringArrCharButt').css("color", "yellow");
+    $('#referenceButt').css("color", "yellow");
+    $('#staticPoleStringuButt').css("color", "yellow");
+    $('#poleAdresPorovnaniButt').css("color", "yellow");
+    $('#referencePoleButt').css("color", "yellow");
+    $('#poleAdres2DPorovnaniButt').css("color", "yellow");
+    $('#poleChar2DPorovnaniAdresButt').css("color", "yellow");
+    $('#staticIntStringCharButt').css("color", "yellow");
+
+}
+
+
+$(document).ready(function(){
+
+    $('#poleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#pole').show();
+        
+    }); 
+
+    $('#forSmyckaPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#forSmyckaPole').show();
+
+    }); 
+
+    $('#velikostPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#velikostPole').show();
+
+    }); 
+
+    $('#pointerNaPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#pointerNaPole').show();
+
+    }); 
+
+    $('#strukturyButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#struktury').show();
+
+    }); 
+
+    $('#strlenSizeofButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#strlenSizeof').show();
+
+    }); 
+
+    $('#substringButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#substring').show();
+
+    });     
+
+    $('#vypisZnakuButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#vypisZnaku').show();
+
+    });     
+
+    $('#zmenaPismeneButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#zmenaPismene').show();
+
+    });   
+
+    $('#poleStrukturButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleStruktur').show();
+
+    });  
+
+    $('#adresyButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#adresy').show();
+
+    });  
+
+    $('#pointerButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#pointer').show();
+
+    });  
+
+    $('#tridaButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#trida').show();
+
+    });  
+
+    $('#tridaMetodaButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaMetoda').show();
+
+    });  
+
+    $('#tridaKonstruktorButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaKonstruktor').show();
+
+    });  
+
+    $('#nactiTxtCislaButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#nactiTxtCisla').show();
+
+    }); 
+
+    $('#tridaGeterButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaGeter').show();
+
+    }); 
+
+    $('#vystupDoTxtButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#vystupDoTxt').show();
+
+    }); 
+
+    $('#tridaSeterButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaSeter').show();
+
+    }); 
+
+    $('#tridaPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaPole').show();
+
+    }); 
+
+    $('#tridaDveTridyButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaDveTridy').show();
+
+    }); 
+
+    $('#tridaKonstructorPointerButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaKonstructorPointer').show();
+
+    });
+
+    $('#tridaPredani2DpoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#tridaPredani2Dpole').show();
+
+    });
+
+    $('#delkaPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#delkaPole').show();
+
+    });
+
+    $('#delkaPole2DButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#delkaPole2D').show();
+
+    });
+
+    $('#delkaPole2DNewButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#delkaPole2DNew').show();
+
+    });
+
+    $('#staticButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#static').show();
+
+    });
+
+    $('#poleMallocButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleMalloc').show();
+
+    });
+
+    $('#staticPoradiVolaniButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#staticPoradiVolani').show();
+
+    });
+
+    $('#staticZapisDoPoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#staticZapisDoPole').show();
+
+    });
+
+    $('#poleZnakuMallocButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleZnakuMalloc').show();
+
+    });
+
+    $('#strlenSizeOfStringArrCharButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#strlenSizeOfStringArrChar').show();
+
+    });
+
+    $('#referenceButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#reference').show();
+
+    });
+
+    $('#staticPoleStringuButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#staticPoleStringu').show();
+
+    });
+
+    $('#poleAdresPorovnaniButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleAdresPorovnani').show();
+
+    });
+
+    $('#referencePoleButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#referencePole').show();
+
+    });
+
+    $('#poleAdres2DPorovnaniButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleAdres2DPorovnani').show();
+
+    });
+
+    $('#poleChar2DPorovnaniAdresButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#poleChar2DPorovnaniAdres').show();
+
+    });
+
+    $('#staticIntStringCharButt').click(function() {
+
+        skryjVsechnyOstatniHtml();
+        $(this).css("color", "red");
+        $('#staticIntStringChar').show();
+
+    });
+
+});
